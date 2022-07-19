43cd255702dd4dbfd3c79f47143fe463f0baec51
Leandro Wagner <lsw_wagner@yahoo.com.br>
2022-07-18 22:40:17 -0300

    adiciona o vendor mf

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b90cec04dd44484cf12d70c9435fb8aa6499aaec',
+    'name' => 'app/mvc',
+  ),
+  'versions' => 
+  array (
+    'app/mvc' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b90cec04dd44484cf12d70c9435fb8aa6499aaec',
+    ),
+  ),
+);
